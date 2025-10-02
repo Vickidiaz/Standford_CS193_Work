601a9d04a5969b4795fa64b8ff56c3371401245f
Vickidiaz <vickidiaz42@gmail.com>
2025-10-01 20:03:11 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by USER1 on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
